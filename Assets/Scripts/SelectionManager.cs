da74d2d0fc9f21ec19413597ca1a06d26f939b9e
Tigran <tikogrig7@gmail.com>
2023-12-09 22:22:29 +0200

    rabbits

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,12 +5,28 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
+    public static SelectionManager Instance { get; set; }
+    public bool onTarget;
+
     public GameObject interaction_Info_UI;
     Text interaction_text;
 
     private void Start()
     {
+        onTarget = false;
         interaction_text = interaction_Info_UI.GetComponent<Text>();
+    }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            Instance = this;
+        }
     }
 
     void Update()
@@ -21,16 +37,31 @@ public class SelectionManager : MonoBehaviour
         {
             var selectionTransform = hit.transform;
 
-            if (selectionTransform.GetComponent<InteractableObject>())
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            if (interactable && interactable.playerInRange)
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+
+                onTarget = true;
+                interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else
             {
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
+
             }
 
+
+        }
+
+        else
+        {
+            onTarget = false;
+            interaction_Info_UI.SetActive(false);
         }
     }
+
+
 }
